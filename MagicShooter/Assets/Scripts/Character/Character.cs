13e74ab8c258e9ffc1ea7ed56c9b07240e2d15e8
Liam Cunneen <67307238+lmcunneen@users.noreply.github.com>
2024-07-23 11:40:59 +1000

    Implemented Ben's AI Fix, Added Working Health Display

--- MagicShooter/Assets/Scripts/Character/Character.cs
+++ MagicShooter/Assets/Scripts/Character/Character.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-
     public CharacterController characterController;
 
     public Weapon currentWeapon;
 
     public int health = 100;
-// Start is called before the first frame update
+
+    private HealthDisplay healthDisplay;
+
     void Start()
     {
         if (!characterController)
             characterController = GetComponent<CharacterController>();
 
+        healthDisplay = FindObjectOfType<HealthDisplay>();
+
+        healthDisplay.UpdateHealthCounter(health);
     }
 
     public void DamagePlayer(int _amountToDamage)
     {
         health -= _amountToDamage;
+        healthDisplay.UpdateHealthCounter(health);
     }
-
-   
 }
---
+++ MagicShooter/Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private TMP_Text display;
+
+    private void Start()
+    {
+        display = GetComponent<TMP_Text>();
+    }
+
+    public void UpdateHealthCounter(int currentHealth)
+    {
+        display.text = "Health: " + currentHealth;
+    }
+}
