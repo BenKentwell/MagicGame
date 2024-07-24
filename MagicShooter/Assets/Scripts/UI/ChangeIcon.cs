8d9a6dd40533153e479f0080b6deeca4ae38777f
MitchellCaldwell29 <131213839+MitchellCaldwell29@users.noreply.github.com>
2024-07-24 12:04:48 +1000

    UI Image Script
    Ready for Ben to look at.

---
+++ MagicShooter/Assets/Scripts/UI/ChangeIcon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeIcon : MonoBehaviour
+{
+    private Image scatter;
+    private Image single;
+    private Image beam;
+
+    private Sprite canvasImage; 
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+        }
+    }
+}
