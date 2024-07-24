0031f18f85be5e0f64a1d88a2ec8df402ded954b
Ben Kentwell <124107785+BenKentwell@users.noreply.github.com>
2024-07-24 15:20:15 +1000

    Build works!

--- MagicShooter/Assets/Scripts/Crystals/CrystalEncounterTrigger.cs
+++ MagicShooter/Assets/Scripts/Crystals/CrystalEncounterTrigger.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 public class CrystalEncounterTrigger : MonoBehaviour
 {
     public CrystalBase crystalToActivate;
-    private bool hasActivated;
+   
 
     private void OnTriggerEnter(Collider _other)
     {
-        if (_other.gameObject.GetComponent<Character>() && !hasActivated)
+        if (_other.gameObject.GetComponent<Character>() )
         {
             crystalToActivate.SetActivation();
-            hasActivated = true;
+            Destroy(gameObject);
         }
     }
 }
